7b989881b8fb8ad05aaa49e1fe512e2912b8b251
rahul8377072330 <40262405+rahul8377072330@users.noreply.github.com>
2018-06-27 17:47:11 +0530

    Update TestNg.java

--- HrisTest/src/test/java/TestNg.java
+++ HrisTest/src/test/java/TestNg.java
@@ -2,11 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import static org.testng.Assert.*;
-import org.testng.annotations.*;
-
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
 public class TestNg{
 	LoginForm hrs=new LoginForm(); 
 	
